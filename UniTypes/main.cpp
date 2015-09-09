d8033558000f9ff00c1027630ba74fc2b6fac7f7
fayaproject <riccardo@MacBook-Pro-di-Riccardo.local>
2015-09-09 11:20:31 +0200

    First commit

---
+++ UniTypes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  UniTypes
+//
+//  Created by Riccardo Cecchini on 09/09/15.
+//  Copyright (c) 2015 Lizard Ink. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
